d0641fc70b9b35f445ef2ccd7208c1e1ee5c15c5
sudhir <sudhirbitsgoa@gmail.com>
2015-02-05 18:18:14 +0530

    adding upload mp4 to gridfs functionality

--- handlers.js
+++ handlers.js
@@ -59,7 +59,7 @@ function _upload(response, file) {
     }
 
     file.contents = file.contents.split(',').pop();
-    var filename = fileRootName+fileExtension;
+    var filename = fileRootName+".mp4" || fileExtension;
 
     fileBuffer = new Buffer(file.contents, "base64");
     var gfs = Grid( conn.db, mongoose.mongo );
@@ -170,10 +170,62 @@ function ifMac(response, files) {
             // removing audio/video files
             //fs.unlink(audioFile);
             fs.unlink(videoFile);
+            storetoGridfs(mergedFile);
         }
 
     });
+    
+
+
 }
+
+
+function storetoGridfs(file){
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Grid = require('mongodb').Grid,
+    Code = require('mongodb').Code,
+    BSON = require('mongodb').pure().BSON,
+    assert = require('assert');
+
+var db = new Db('test', new Server('localhost', 27017));
+// Establish connection to db
+db.open(function(err, db) {
+  // Our file ID
+  var fileId = new ObjectID();
+
+  // Open a new file
+  var gridStore = new GridStore(db, fileId, 'w');
+
+  // Read the filesize of file on disk (provide your own)
+  var fileSize = fs.statSync(file || './test/tests/functional/gridstore/test_gs_weird_bug.png').size;
+  // Read the buffered data for comparision reasons
+  var data = fs.readFileSync(file || './test/tests/functional/gridstore/test_gs_weird_bug.png');
+
+  // Open the new file
+  gridStore.open(function(err, gridStore) {
+
+    // Write the file to gridFS
+    gridStore.writeFile(file || './test/tests/functional/gridstore/test_gs_weird_bug.png', function(err, doc) {
+
+      // Read back all the written content and verify the correctness
+      GridStore.read(db, fileId, function(err, fileData) {
+        //assert.equal(data.toString('base64'), fileData.toString('base64'))
+        //assert.equal(fileSize, fileData.length);
+
+        db.close();
+      });
+    });
+  });
+});
+}
+
+
 
 exports.home = home;
 exports.upload = upload;
